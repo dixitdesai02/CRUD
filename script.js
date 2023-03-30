96cff5e9f24c492b00b8fe02ac68d413614eeb4f
Dixit Desai <dixit.d@simformsolutions.com>
2023-03-30 18:31:02 +0530

    Add Edit Function and Made some UI changes

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const form = document.querySelector("#AddProduct form");
-const id = form.querySelector("#product-id");
-const name = form.querySelector("#name");
-const price = form.querySelector("#price");
-const desc = form.querySelector("#description");
-const imageInput = form.querySelector("#image");
-const outputImage = form.querySelector("#output");
+const addBtn = document.getElementById("add-btn");
+const addProduct = document.getElementById("AddProduct");
+
+addBtn.addEventListener("click", () => {
+    addProduct.classList.toggle("show");
+})
+
+
+const addForm = document.querySelector("#AddProduct form");
+const id = addForm.querySelector("#product-id");
+const name = addForm.querySelector("#name");
+const price = addForm.querySelector("#price");
+const desc = addForm.querySelector("#description");
+const imageInput = addForm.querySelector("#image");
+const outputImage = addForm.querySelector("#output");
 
 const productSection = document.querySelector("#Products .container");
 
@@ -17,29 +25,18 @@ function loadProducts() {
     products.forEach((product) => {
 
         const productItem = document.createElement("div");
+        productItem.setAttribute("href", `productDetail.html?id=${product.id}`);
         productItem.classList.add("card");
-        productItem.innerHTML += ` <img src="${product.image || 'images/dummy-image.png'}" class="card-img-top" alt="...">
+        productItem.innerHTML += ` <a href="productDetail.html?id=${product.id}"> <img src="${product.image || 'images/dummy-image.png'}" class="card-img-top" alt="..."> </a>
                                     <div class="card-body">
-                                    <h5 class="card-title">${product.id}. ${product.name}</h5>
-                                    <p class="card-desc">${product.description}</p>
+                                    <a href="productDetail.html?id=${product.id}"> <h5 class="card-title">${product.id}. ${product.name.length > 15 ? product.name.slice(0,15) + "...": product.name}</h5> </a>
+                                    <p class="card-desc opacity-75">${product.description.length > 50 ? product.description.slice(0,50) + "...": product.description}</p>
                                     <p class="card-price">₹ ${product.price} <span>/-</span> </p>
-                                    <button class="btn btn-sm btn-warning edit" data-bs-toggle="modal" data-bs-target="#modal">Edit</button>
-                                    <button class="btn btn-sm btn-danger delete">Delete</button>
+                                    <button class="btn btn-sm btn-warning edit" data-bs-toggle="modal" data-bs-target="#modal" onClick={editProduct(${product.id})}>Edit</button>
+                                    <button class="btn btn-sm btn-danger delete" onClick={deleteProduct(${product.id})}>Delete</button>
                                 </div>`;
         
         productSection.append(productItem);
-
-        // Delete Event
-        const deleteBtn = productItem.querySelector(".delete");
-        deleteBtn.addEventListener("click", (e) => {
-            deleteProduct(e);
-        });
-
-        // Edit Event
-        const editBtn = productItem.querySelector(".edit");
-        editBtn.addEventListener("click", (e) => {
-            editProduct(e);
-        });
 
     });
 }
@@ -66,7 +63,7 @@ imageInput.addEventListener('change', (event) => {
 });
 
 
-form.addEventListener("submit", (e) => {
+addForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     product.id = id.value;
@@ -93,57 +90,69 @@ form.addEventListener("submit", (e) => {
 
     }
     catch(error) {
-        showNotification();
+        showNotification("Error");
     }
 
 });
 
-function showNotification() {
+function showNotification(msg) {
     const notification = document.querySelector(".notification");
 
-    notification.style.display = "flex";
+    notification.innerHTML = "";
+
+    const notificationText = document.createElement("div");
+    notificationText.classList.add("d-flex", "justify-content-between", "align-items-center");
+    notification.append(notificationText);
+
+    if (msg === "Error") {
+        notification.style.backgroundColor = 'rgba(220,53,69,0.9)';
+        notificationText.innerHTML = `<p>Storage is Full! Delete some items to Add more..</p> <i class="close fa-solid fa-xmark fs-4"></i>`;
+    }
+    else if (msg === "Edited") {
+        notification.style.backgroundColor = '#318481';
+        notificationText.innerHTML = `<p>Hurrey!! Edited Successfully!</p> <i class="close fa-solid fa-xmark fs-4"></i>`;
+    }
+    else if (msg === "Duplicate") {
+        notification.style.backgroundColor = 'rgba(220,53,69,0.9)';
+        notificationText.innerHTML = `<p>Product ID should be Unique!!</p> <i class="close fa-solid fa-xmark fs-4"></i>`;
+    }
+
+    notification.style.display = "block";
 
     setTimeout(()=>{
         notification.style.display = "none";
     }, 4000);
+
+    const closeNotification = notification.querySelector(".close");
+    closeNotification.addEventListener("click", ()=>{
+        notification.style.display = "none";
+    });
+    
 };
 
 function AddProduct(product) {
-    const productItem = document.createElement("div");
+    const productItem = document.createElement("a");
+    productItem.setAttribute("href", "productDetail.html");
     productItem.classList.add("card");
     productItem.innerHTML = ` <img src="${product.image || 'images/dummy-image.png'} " class="card-img-top" alt="...">
                                 <div class="card-body">
-                                <h5 class="card-title">${product.id}. ${product.name}</h5>
-                                <p class="card-desc">${product.description}</p>
+                                <h5 class="card-title">${product.id}. ${product.name.length > 15 ? product.name.slice(0,15) + "...": product.name}</h5>
+                                <p class="card-desc">${product.description.length > 50 ? product.description.slice(0,50) + "...": product.description}</p>
                                 <p class="card-price">₹ ${product.price} <span>/-</span> </p>
-                                <button class="btn btn-sm btn-warning edit"  data-bs-toggle="modal" data-bs-target="#modal">Edit</button>
-                                <button class="btn btn-sm btn-danger delete">Delete</button>
+                                <button class="btn btn-sm btn-warning edit" data-bs-toggle="modal" data-bs-target="#modal" onClick={editProduct(${product.id})}>Edit</button>
+                                <button class="btn btn-sm btn-danger delete" onClick={deleteProduct(${product.id})}>Delete</button>
                             </div>`;
     
     productSection.append(productItem);
-    
-    // Delete Event
-    const deleteBtn = productItem.querySelector(".delete");
-    deleteBtn.addEventListener("click", (e) => {
-        deleteProduct(e);
-    });
-
-    // Edit Event
-    const editBtn = productItem.querySelector(".edit");
-    editBtn.addEventListener("click", (e) => {
-        editProduct(e);
-    });
 
 }
 
 // Edit Operation
-function editProduct(event) {
-    const productTitle = event.target.parentElement.childNodes[1].textContent;
-    const index = productTitle.indexOf(".");
-    const id = productTitle.slice(0, index);
-    
+function editProduct(id) {
+
+    console.log("Edit here");
     const productList = JSON.parse(localStorage.getItem("products"));
-    const productToBeUpdated = productList.find(product => product.id === id);
+    const productToBeUpdated = productList.find(product => product.id == id);
     console.log(productToBeUpdated);
 
     const editForm = document.querySelector("#modal form");
@@ -181,30 +190,36 @@ function editProduct(event) {
     });
 
 
-    editForm.addEventListener("click", (e) => {
+    editForm.addEventListener("submit", (e) => {
         e.preventDefault();
 
+        editedProduct.id = id + "";
         editedProduct.name = name.value;
         editedProduct.price = price.value;
         editedProduct.description = desc.value;
 
-        const foundIndex = productList.findIndex(product => product.id === id);
-        console.log(foundIndex);
-        // productList[foundIndex] = editProduct;
+        const foundIndex = productList.findIndex(product => product.id == id);
 
-        // localStorage.setItem("products", JSON.stringify(productList));
+        if (!editedProduct.image)
+            editedProduct.image = productList[foundIndex].image;
+
+        productList[foundIndex] = editedProduct;
+
+        localStorage.setItem("products", JSON.stringify(productList));
+        
+        showNotification("Edited");
+
+        loadProducts();
+
     })
 }
 
 
 // Delete Operation
-function deleteProduct(event) {
-
-    const productTitle = event.target.parentElement.childNodes[1].textContent;
-    const index = productTitle.indexOf(".");
-    const id = productTitle.slice(0, index);
+function deleteProduct(id) {
 
     const products = JSON.parse(localStorage.getItem("products"));
+    console.log(`id${id}`);
 
     swal({
         title: "Are you sure?",
@@ -215,11 +230,78 @@ function deleteProduct(event) {
       })
       .then((willDelete) => {
         if (willDelete) {
-            const updatedProducts = products.filter(product => product.id !== id);
+            const updatedProducts = products.filter(product => product.id != id);
+
+            console.log(updatedProducts);
 
             localStorage.setItem("products", JSON.stringify(updatedProducts));
             
             loadProducts();
         }
     });
+}
+
+const sort = document.querySelector(".sort-selector");
+const order = document.querySelector(".order-selector");
+
+sort.addEventListener("change", () => {
+    sortProducts();
+});
+
+order.addEventListener("change", () => {
+    sortProducts();
+    console.log("INside order");
+});
+
+function sortProducts() {
+    const sortBy = sort.value;
+    const sortOrder = order.value;
+    console.log("Sort by", sortBy);
+    console.log(sortOrder);
+
+    const products = JSON.parse(localStorage.getItem("products")) || [];
+    console.log(products);
+
+    if (sortOrder === "asc") {
+        if (sortBy === "name")
+            products.sort((a,b) => a.name.localeCompare(b.name));
+        else
+            products.sort((a,b) => a[sortBy] - b[sortBy]);
+    }
+    else {
+        if (sortBy === "name")
+            products.sort((a,b) => b.name.localeCompare(a.name));
+        else
+            products.sort((a,b) => b[sortBy] - a[sortBy]);
+    }
+
+
+    localStorage.setItem("products", JSON.stringify(products));
+
+    loadProducts();
+}
+
+
+const searchFilter = document.querySelector(".search");
+console.log(searchFilter);
+searchFilter.addEventListener("input", () => {
+    filterProducts();    
+})
+
+function filterProducts() {
+    const searchTerm = searchFilter.value;
+
+    const products = JSON.parse(localStorage.getItem("products")) || [];
+
+    const searchResult = [];
+
+    for (let product of products) {
+        if (product.id.includes(searchTerm)) {
+            searchResult.push(product);
+        }
+        else if (product.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+            searchResult.push(product);
+        }
+    }
+    
 }
